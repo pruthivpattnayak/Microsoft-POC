40e86d0a0e161c418296307f957ad5d90d167cbf
pruthivpattnayak <62070841+pruthivpattnayak@users.noreply.github.com>
2021-08-13 13:25:46 +0530

    Updated code
    Take pull of the updated code

--- src/Tiles.js
+++ src/Tiles.js
@@ -4,12 +4,96 @@ import "./Tile.css";
 export default class Tiles extends Component{
     render(){
         return(
-            <div className="tileContainer">
-                <div className="tileBox1">Get Started with Q&A</div>
-                <div className="tileBox2">Top Microsoft Q&A features</div>
-                <div className="tileBox3">Guidelines for writing a quality question</div>
-                <div className="tileBox4">Write a quality answer</div>
+            <div>
+                <div className="tileContainer">
+                    <div className="box">Get Started with Q&A</div>
+                    <div className="box">Top Microsoft Q&A features</div>
+                    <div className="box">Guidelines for writing a quality question</div>
+                    <div className="box">Write a quality answer</div>
+                </div>
+                <div className="parent">
+                    <div className="supportConatiner">
+                        <div  className="textContainer">Supported products</div>
+                    </div>
+                    <div className="supporttileContainer">
+                        <div className="box">.NET
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                        <div className="box">Azure
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                        <div className="box">BizTalk Server
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                        <div className="box">C++
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                    </div>
+
+                    <div className="supporttileContainer">
+                        <div className="box">Enterprise Mobility + Security
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                        <div className="box">Microsoft 365
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                        <div className="box">Microsoft Advertising
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                        <div className="box">Microsoft Edge
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                    </div>
+
+                    <div className="supporttileContainer">
+                        <div className="box">Microsoft Robotics
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                        <div className="box">Open Specifications
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                        <div className="box">Power Query
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                        <div className="box">Small Basic
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                    </div>
+
+                    <div className="supporttileContainer">
+                        <div className="box">SQL Server
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                        <div className="box">System Center
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                        <div className="box">Visual Studio
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                        <div className="box">Windows
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div>
+                    </div>
+
+                    <div className="supporttileContainer">
+                        <div className="box-single">Windows Sysinternals
+                            <p>Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more!</p>
+                        </div> 
+                    </div>
+                </div>
+
+                <div className="footer">
+                    <div>English(United States)</div>                    
+                </div>
+                <div className="footer2">
+                    <div>Previous Version Docs</div>
+                    <div>Blog</div>                 
+                    <div>Contribute</div> 
+                    <div>Privacy & Cookie</div> 
+                    <div>Microsoft 2021</div>
+                </div>
             </div>
+
         )
     }
 }
